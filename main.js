4848925f16c8739320e52b4b1bc23aabda3087eb
Nihal-cyber <79103803+Nihal-cyber@users.noreply.github.com>
2022-04-06 19:37:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+img = "";
+function preload(){
+    img= loadImage('dog_cat.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("#000000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#000000");
+    rect(30, 60, 450, 350);
+
+}
